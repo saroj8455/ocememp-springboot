b04827c8056d1d6b7009c5265011cfe38a8f095b
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-10-01 01:12:35 +0530

    ready project to write controller and end point for intelij idea

---
+++ src/main/java/com/dev/ocem/OcemempApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ocem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcemempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcemempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/ocem/OcemempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ocem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcemempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
